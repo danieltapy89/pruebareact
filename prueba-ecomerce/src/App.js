732be2147663c615b939f1eb7dcf040dcb8e0c6e
marianosuarez80 <ms@MacBook-Pro-de-Mariano.local>
2022-08-10 19:50:36 -0300

    primer commit

---
+++ prueba-ecomerce/src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import Product from './components/Product';
+import Navbar from './components/Navbar';
+import Products from './components/Products';
+import CheckoutPage from './components/CheckoutPage';
+import CheckoutCard from './components/CheckoutCard';
+import { Switch, BrowserRouter as Router, Route } from "react-router-dom";
+
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Switch>
+          <Route path='/checkout-page'>
+            <CheckoutPage />
+          </Route>
+          <Route path='/'>
+            <Products />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
